02d2d6cf0f53607a29550fd3960badb5fbdd810f
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2022-03-14 20:52:33 +0800

    Create navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css'
 import { BrowserRouter as Router } from 'react-router-dom'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
     return (
         <Router basename={process.env.PUBLIC_URL}>
-            
+            <Navbar />
         </Router>
     )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 // import { bubble as Menu } from "react-burger-menu"
 import './Navbar.css'
 
-import logo from '../../images/logo.png'
+import logo from '../../images/logo-full.png'
 import iconTwitter from '../../images/Twitter-Pixel.png'
 import iconInstagram from '../../images/IG-Pixel.png'
 import iconDiscord from '../../images/Discord-Pixel.png'
@@ -75,64 +75,66 @@ export default function Navbar(props) {
     // } 
 
     return (
-        <div id="header" className="container">
-            <div id="punks-nav" className="d-none d-lg-flex justify-content-between align-items-center">
-                <div className="punks-nav-logo">
-                    <Link exact="true" to="/">
-                        <img src={logo} className="w-100" alt="CyberPunks Logo" />
-                    </Link>
-                </div>
-                {/* <div classname="d-flex justify-content-end align-items-center"> */}
-                    <ul className="d-flex justify-content-center align-items-center mb-0 p-0">
-                        <li>
-                            <a className="punks-nav-icon-link" href="" target="_blank" rel="noreferrer">
-                                <img className="w-100" src={iconTwitter} alt="CyberPunks Twitter" />
-                            </a>
-                        </li>
-                        <li>
-                            <HashLink smooth to="#updates" scroll={el => scrollWithOffset(el)}>UPDATES</HashLink>
-                        </li>
-                        <li>
-                            <HashLink smooth to="#roadmap" scroll={el => scrollWithOffset(el)}>ROADMAP</HashLink>
-                        </li>
-                        <li>
-                            <HashLink smooth to="#team" scroll={el => scrollWithOffset(el)}>TEAM</HashLink>
-                        </li>
-                        <li>
-                            <HashLink smooth to="#sneakpeek" scroll={el => scrollWithOffset(el)}>PROGRESS</HashLink>
-                        </li>
-                        <li>
-                            <HashLink smooth to="#faqs" scroll={el => scrollWithOffset(el)}>FAQS</HashLink>
-                        </li>
-                        <li>
-                            <button type="button" onClick={props.mintBtn} className="btn mq-nav-discord text-white gotham-black font-size-100">
+        <div id="header">
+            <div className="container">
+                <div id="punks-nav" className="d-none d-lg-flex justify-content-between align-items-center">
+                    <div className="punks-nav-logo-wrap">
+                        <div className="punks-nav-logo">
+                            <Link exact="true" to="/">
+                                <img src={logo} className="punks-nav-logo" alt="CyberPunks Logo" />
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="punks-nav-other">
+                        <div className="punks-socmed d-flex justify-content-end align-items-center">
+                            <div className="punks-socmed-item">
+                                <a href="https://twitter.com/NFTCyberPunks">
+                                    <img src={iconTwitter} className="w-100" alt="CyberPunks 2088 Twitter" />
+                                </a>
+                            </div>
+                            <div className="punks-socmed-item">
+                                <a href="https://instagram.com/nftcyberpunks/">
+                                    <img src={iconInstagram} className="w-100" alt="CyberPunks 2088 Instagram" />
+                                </a>
+                            </div>
+                            <div className="punks-socmed-item">
+                                <a href="https://discord.gg/PC3bthGRWg">
+                                    <img src={iconDiscord} className="w-100" alt="CyberPunks 2088 Discord" />
+                                </a>
+                            </div>
+                            <div className="punks-socmed-item">
+                                <a href="https://opensea.io/collection/cyberpunks2088">
+                                    <img src={iconOpensea} className="w-100" alt="CyberPunks 2088 OpenSea" />
+                                </a>
+                            </div>
+                            <button className="btn punks-nav-btn btn-custom-1 py-1 px-4 font-size-180">
                                 MINT NOW
                             </button>
-                        </li>
-                    </ul>
-                {/* </div> */}
-            </div>
-            {/* <div className="d-lg-none align-self-end">
-                <Menu right styles={styles}
-                    isOpen={menuOpenState}
-                    onStateChange={(state) => handleStateChange(state)}
-                >
-                    <div className="mq-nav-logo-mobile">
-                        <Link exact="true" to="/">
-                            <img src={logo} className="w-100" alt="Mustachio Quest Logo" />
-                        </Link>
+                        </div>
                     </div>
-                    <HashLink smooth to="#features" scroll={el => scrollWithOffset(el)} onClick={() => closeMenu()} className="text-black d-block py-3 w-100 text-decoration-none">FEATURES</HashLink>
-                    <HashLink smooth to="#updates" scroll={el => scrollWithOffset(el)} onClick={() => closeMenu()} className="text-black d-block py-3 w-100 text-decoration-none">UPDATES</HashLink>
-                    <HashLink smooth to="#roadmap" scroll={el => scrollWithOffset(el)} onClick={() => closeMenu()} className="text-black d-block py-3 w-100 text-decoration-none">ROADMAP</HashLink>
-                    <HashLink smooth to="#team" scroll={el => scrollWithOffset(el)} onClick={() => closeMenu()} className="text-black d-block py-3 w-100 text-decoration-none">TEAM</HashLink>
-                    <HashLink smooth to="#sneakpeek" scroll={el => scrollWithOffset(el)} onClick={() => closeMenu()} className="text-black d-block py-3 w-100 text-decoration-none">PROGRESS</HashLink>
-                    <HashLink smooth to="#faqs" scroll={el => scrollWithOffset(el)} onClick={() => closeMenu()} className="text-black d-block py-3 w-100 text-decoration-none">FAQS</HashLink>
-                    <button type="button" onClick={props.mintBtn} className="btn mq-nav-discord text-white gotham-black font-size-100">
-                        MINT NOW
-                    </button>
-                </Menu>
-            </div> */}
+                </div>
+                {/* <div className="d-lg-none align-self-end">
+                    <Menu right styles={styles}
+                        isOpen={menuOpenState}
+                        onStateChange={(state) => handleStateChange(state)}
+                    >
+                        <div className="mq-nav-logo-mobile">
+                            <Link exact="true" to="/">
+                                <img src={logo} className="w-100" alt="Mustachio Quest Logo" />
+                            </Link>
+                        </div>
+                        <HashLink smooth to="#features" scroll={el => scrollWithOffset(el)} onClick={() => closeMenu()} className="text-black d-block py-3 w-100 text-decoration-none">FEATURES</HashLink>
+                        <HashLink smooth to="#updates" scroll={el => scrollWithOffset(el)} onClick={() => closeMenu()} className="text-black d-block py-3 w-100 text-decoration-none">UPDATES</HashLink>
+                        <HashLink smooth to="#roadmap" scroll={el => scrollWithOffset(el)} onClick={() => closeMenu()} className="text-black d-block py-3 w-100 text-decoration-none">ROADMAP</HashLink>
+                        <HashLink smooth to="#team" scroll={el => scrollWithOffset(el)} onClick={() => closeMenu()} className="text-black d-block py-3 w-100 text-decoration-none">TEAM</HashLink>
+                        <HashLink smooth to="#sneakpeek" scroll={el => scrollWithOffset(el)} onClick={() => closeMenu()} className="text-black d-block py-3 w-100 text-decoration-none">PROGRESS</HashLink>
+                        <HashLink smooth to="#faqs" scroll={el => scrollWithOffset(el)} onClick={() => closeMenu()} className="text-black d-block py-3 w-100 text-decoration-none">FAQS</HashLink>
+                        <button type="button" onClick={props.mintBtn} className="btn mq-nav-discord text-white gotham-black font-size-100">
+                            MINT NOW
+                        </button>
+                    </Menu>
+                </div> */}
+            </div>
         </div>
     )
 }
